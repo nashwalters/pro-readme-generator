3ccbf2441f5a04c9e5da789354447dd3f662c439
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-01-23 19:14:00 -0500

    Modified question/prompts.

--- index.js
+++ index.js
@@ -15,59 +15,59 @@ inquirer.prompt([
         type: 'input',
         name: 'name',
         message: 'What is the name of the author of the project?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'title',
         message: 'What is the title of the project?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'description',
-        message: 'Write a brief description of the project.',
-      },
-      {
+        message: 'Write a description of the project.',
+    },
+    {
         type: 'input',
         name: 'install',
         message: 'What are the steps required for installion?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'usage',
         message: 'How can the project be used?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'test',
         message: 'What tests have you written for this README?',
-      },
-      {
+    },
+    {
         type: 'list',
         name: 'contributors',
         message: 'Are you accepting contributors?',
         choices: ["Contributions are accepted", "Contributions are not accepted at this time"],
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'contribute',
         message: 'How can people contribute (if contributions are not accepted leave blank)?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub Username',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'email',
         message: 'Enter your email address.',
-      },
-      {
+    },
+    {
         type: 'list',
         name: 'license',
         message: 'What kind of license should your project have?',
         choices: ["Apache-2.0", "MIT", "ISC", "GPL", "Artistic-2.0","wtfpl"],
-      },
+    },
 
 ]);
 
