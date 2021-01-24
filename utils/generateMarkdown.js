f20214cb9221e17a87ecc4895429f5cb81204db3
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-01-23 19:22:46 -0500

    Format markdown.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![Licence:${data.license}](https://img.shields.io/badge/license-${data.license}-blue.svg "license Badge")
 
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+## Description
+${data.description}
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributors}<br>
+${data.contribute}
+
+## Tests
+${data.test}
+
+## Questions
+Contact me with any questions:
+* Email: ${data.email}
+* Github: <a href="https://github.com/${data.github}">${data.github}</a>
+
+## License
+Copyright © 2021, ${data.name}.<br>Released under the <a href="https://opensource.org/licenses/${data.license}">${data.license}</a> license.
 `;
 }
 
